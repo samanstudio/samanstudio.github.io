31b5b4a9f4ef0190c5fe3a3d716cfc1b155a2eee
samanstudio <saman1986a@gmail.com>
2025-05-20 12:33:18 -0700

    Try to add 2nd bg video and fail miserably

--- js/backgroundvideos.js
+++ js/backgroundvideos.js
@@ -1,8 +1,16 @@
-const container = document.querySelector('.hero-img');
-const video = document.querySelector('video#hero-video');
+// const container = document.querySelector('.hero-img');
+// const video = document.querySelector('video#hero-video');
+
+const videoContainers = document.getElementsByClassName('bg-video-container');
+const videos = document.getElementsByClassName('bg-video');
 
 window.addEventListener('scroll', () => {
-    const rect = container.getBoundingClientRect();
-    const offset = -rect.top;
-    video.style.transform = `translateY(${offset}px)`;
+    for (let vc = 0; vc < videoContainers.length; vc++) {
+        const rect = videoContainers[vc].getBoundingClientRect();
+        const offset = -rect.top;
+        videos[vc].style.transform = `translateY(${offset}px)`;
+    }
+    // const rect = container.getBoundingClientRect();
+    // const offset = -rect.top;
+    // video.style.transform = `translateY(${offset}px)`;
 });
